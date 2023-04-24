be5319e6cfedd5d5b56553d7c8f357a26853965f
thappatan chanphen <thappatan.work@gmail.com>
2023-04-24 13:37:13 +0700

    edit: update more detail

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,6 @@ module.exports = {
     logs: true,
     rtl: false,
     prefix: "",
-    themes: ["forest"], //"light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter",
+    themes: ["forest"],
   },
 };
